58e4d6b7282e31adacf8a08a0c09d4e8a8bb866a
KobiManuel <136437741+EmmanuelKobi@users.noreply.github.com>
2023-07-31 13:08:51 +0100

    feat: modified DevTools page

--- src/Pages/DeveloperPage/DevTools.jsx
+++ src/Pages/DeveloperPage/DevTools.jsx
@@ -1,36 +1,40 @@
-import React, { useContext, useRef } from "react";
+import React, { useRef } from "react";
 import "./_DevTools.scss";
-import { PortfolioContext } from "../../Context/context";
 import LunaCodex from "./LunaCodex/Main";
 import Hero from "./Hero/Main";
 import SynthSpeak from "./SynthSpeak/Main";
 import SvgConverter from "./SvgConverter/Main";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setFirstCard,
+  setSecondCard,
+  setThirdCard,
+  setHeroCard,
+} from "../../Context/portfolioSlice";
 
 const DeveloperPage = () => {
-  const {
-    heroCard,
-    firstCard,
-    secondCard,
-    thirdCard,
-    setFirstCard,
-    setSecondCard,
+  const dispatch = useDispatch();
 
-    setThirdCard,
-    setHeroCard,
-  } = useContext(PortfolioContext);
+  const heroCard = useSelector((state) => state.portfolio.heroCard);
+  const firstCard = useSelector((state) => state.portfolio.firstCard);
+  const secondCard = useSelector((state) => state.portfolio.secondCard);
+  const thirdCard = useSelector((state) => state.portfolio.thirdCard);
+
   const cardValues = [heroCard, firstCard, secondCard, thirdCard];
-  const cardSetters = [setHeroCard, setFirstCard, setSecondCard, setThirdCard];
 
   const handleCardClick = (index) => {
-    cardSetters.forEach((setter, i) => {
-      setter(i === index);
-    });
+    dispatch(setHeroCard(index === 0));
+    dispatch(setFirstCard(index === 1));
+    dispatch(setSecondCard(index === 2));
+    dispatch(setThirdCard(index === 3));
+
     const pagesContainer = devToolsRef.current.querySelector(".devtool-pages");
     const component = pagesContainer.children[index];
     if (component) {
       component.scrollIntoView({ behavior: "smooth", block: "start" });
     }
   };
+
   const devToolsRef = useRef(null);
 
   return (
