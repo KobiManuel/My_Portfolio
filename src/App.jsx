e76c842ac868a393c546b4d5e9ac076bddb0f22f
KobiManuel <136437741+EmmanuelKobi@users.noreply.github.com>
2023-09-19 08:07:37 +0100

    feat: minor change

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,33 @@ import DeveloperPage from "./Pages/DeveloperPage/DevTools";
 import FrontendPage from "./Pages/FrontendPage/FrontendPage";
 import DesignPortfolio from "./Pages/DesignPortfolio/DesignPortfolio";
 import AboutMe from "./Pages/AboutMe/AboutMe";
+import { useEffect, useRef } from "react";
 
 function App() {
+  const containerRef = useRef(null);
+  useEffect(() => {
+    const container = containerRef.current;
+
+    const handleMouseMove = (event) => {
+      const { clientX, clientY } = event;
+      const offsetX = ((clientX / window.innerWidth) * 60).toFixed(2);
+      const offsetY = ((clientY / window.innerHeight) * 60).toFixed(2);
+
+      container.style.setProperty(
+        "background-position",
+        `calc(130% + ${offsetX}px) calc(50% + ${offsetY}px)`
+      );
+    };
+
+    container.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      container.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
-    <div className="App">
+    <div className="App" ref={containerRef}>
       <Router>
         <Routes>
           <Route path="/" element={<MainPage />} />
