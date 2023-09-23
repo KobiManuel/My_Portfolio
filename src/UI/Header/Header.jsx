42c40f244c62fc28ba805d201a8f5dd893422d43
KobiManuel <136437741+EmmanuelKobi@users.noreply.github.com>
2023-09-23 21:35:25 +0100

    feat: updated button to link

--- src/UI/Header/Header.jsx
+++ src/UI/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({ subPage }) => {
   return (
     <header>
       {subPage && window.innerWidth < 1000 ? (
-        <span className="backward-svg">
+        <Link to="/" className="backward-svg">
           <svg
             className="backward-svg-arrow"
             xmlns="http://www.w3.org/2000/svg"
@@ -16,7 +16,7 @@ const Header = ({ subPage }) => {
               <path d="M32,15H3.41l8.29-8.29L10.29,5.29l-10,10a1,1,0,0,0,0,1.41l10,10,1.41-1.41L3.41,17H32Z" />
             </g>
           </svg>
-        </span>
+        </Link>
       ) : (
         ""
       )}
